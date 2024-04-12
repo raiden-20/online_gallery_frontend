5d6fe3d0383405e48fb2d63e09403986ad6f9967
korobasssss <an.korobeynikova@gmail.com>
2024-04-12 10:02:02 +0300

    design works in profile

--- src/components/profile/profile_elemets/categories/works/WorksProfileComponent.tsx
+++ src/components/profile/profile_elemets/categories/works/WorksProfileComponent.tsx
@@ -1,6 +1,86 @@
+import works_profile_scss from '@/scss/components/profile/categories/WorksProfile.module.scss'
+
 export const WorksProfileComponent = () => {
+
     return (
-        <ul>
+        <ul className={works_profile_scss.root}>
+            <li>
+                <section className={works_profile_scss.one_work}>
+                    <img src={'/default_work_profile.jpg'} className={works_profile_scss.one_work_img}
+                         alt={'one work'}/>
+                    <section className={works_profile_scss.one_work_names}>
+                        <div className={works_profile_scss.one_work_weight}>Имя</div>
+                        <div>Название</div>
+                    </section>
+                    <div className={works_profile_scss.one_work_weight}>100000 ₽</div>
+                </section>
+            </li>
+            <li>
+                <section className={works_profile_scss.one_work}>
+                    <img src={'/default_work_profile.jpg'} className={works_profile_scss.one_work_img}
+                         alt={'one work'}/>
+                    <section className={works_profile_scss.one_work_names}>
+                        <div className={works_profile_scss.one_work_weight}>Имя</div>
+                        <div>Название</div>
+                    </section>
+                    <div className={works_profile_scss.one_work_weight}>100000 ₽</div>
+                </section>
+            </li>
+            <li>
+                <section className={works_profile_scss.one_work}>
+                    <img src={'/default_work_profile.jpg'} className={works_profile_scss.one_work_img}
+                         alt={'one work'}/>
+                    <section className={works_profile_scss.one_work_names}>
+                        <div className={works_profile_scss.one_work_weight}>Имя</div>
+                        <div>Название</div>
+                    </section>
+                    <div className={works_profile_scss.one_work_weight}>100000 ₽</div>
+                </section>
+            </li>
+            <li>
+                <section className={works_profile_scss.one_work}>
+                    <img src={'/default_work_profile.jpg'} className={works_profile_scss.one_work_img}
+                         alt={'one work'}/>
+                    <section className={works_profile_scss.one_work_names}>
+                        <div className={works_profile_scss.one_work_weight}>Имя</div>
+                        <div>Название</div>
+                    </section>
+                    <div className={works_profile_scss.one_work_weight}>100000 ₽</div>
+                </section>
+            </li>
+            <li>
+                <section className={works_profile_scss.one_work}>
+                    <img src={'/default_work_profile.jpg'} className={works_profile_scss.one_work_img}
+                         alt={'one work'}/>
+                    <section className={works_profile_scss.one_work_names}>
+                        <div className={works_profile_scss.one_work_weight}>Имя</div>
+                        <div>Название</div>
+                    </section>
+                    <div className={works_profile_scss.one_work_weight}>100000 ₽</div>
+                </section>
+            </li>
+            <li>
+                <section className={works_profile_scss.one_work}>
+                <img src={'/default_work_profile.jpg'} className={works_profile_scss.one_work_img}
+                         alt={'one work'}/>
+                    <section className={works_profile_scss.one_work_names}>
+                        <div className={works_profile_scss.one_work_weight}>Имя</div>
+                        <div>Название</div>
+                    </section>
+                    <div className={works_profile_scss.one_work_weight}>100000 ₽</div>
+                </section>
+            </li>
+            <li>
+                <section className={works_profile_scss.one_work}>
+                    <img src={'/default_work_profile.jpg'} className={works_profile_scss.one_work_img}
+                         alt={'one work'}/>
+                    <section className={works_profile_scss.one_work_names}>
+                        <div className={works_profile_scss.one_work_weight}>Имя</div>
+                        <div>Название</div>
+                    </section>
+                    <div className={works_profile_scss.one_work_weight}>100000 ₽</div>
+                </section>
+            </li>
 
         </ul>
     )

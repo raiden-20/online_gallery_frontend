240f49f1ab142e3b817ef990b06e46c64d128863
korobasssss <an.korobeynikova@gmail.com>
2024-04-16 19:51:50 +0300

    design for opened post photo

--- src/components/profile/profile_elemets/categories/artist/posts/PostsArtistComponent.tsx
+++ src/components/profile/profile_elemets/categories/artist/posts/PostsArtistComponent.tsx
@@ -1,17 +1,20 @@
 import posts_artist_module from '@/scss/components/profile/categories/PostsArtist.module.scss'
+import {
+    OnePostArtistComponent
+} from "@/components/profile/profile_elemets/categories/artist/posts/OnePostArtistComponent";
 
 export const PostsArtistComponent = () => {
 
     return (
         <ul className={posts_artist_module.root}>
             <li>
-                <section className={posts_artist_module.one_post + ' ' + posts_artist_module.border}>
-                    <img src={'/default_work_profile.jpg'} className={posts_artist_module.one_post_img}
-                         alt={'post photo'}/>
-                    <header className={posts_artist_module.one_post_title}>Заголовок</header>
-                    <p>Оцените этот нереальный рисунок карандашом</p>
-                    <footer className={posts_artist_module.one_post_date}>9 апреля, 02:33</footer>
-                </section>
+                <OnePostArtistComponent/>
+            </li>
+            <li>
+                <OnePostArtistComponent/>
+            </li>
+            <li>
+                <OnePostArtistComponent/>
             </li>
         </ul>
     )

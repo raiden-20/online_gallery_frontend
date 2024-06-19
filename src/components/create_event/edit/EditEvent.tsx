f9d0a22268923b5fa3d16282deb167f454818769
korobasssss <an.korobeynikova@gmail.com>
2024-06-20 00:47:44 +0300

    edit edit event

--- src/components/create_event/edit/EditEvent.tsx
+++ src/components/create_event/edit/EditEvent.tsx
@@ -22,65 +22,66 @@ export const EditEvent = (props: EditEventInterface) => {
         props.GetOneEvent(eventId, Cookies.get('customerId') as string)
     }, []);
 
+    const eventUrl = usePathname().split('/')[usePathname().split('/').length - 1]
+
+    const [photoArraySrcEvent, setPhotoArraySrcEvent] = useState<string[]>([])
+    const [photoArrayFileEvent, setPhotoArrayFileEvent] = useState<File[]>([])
+    const [deleteEventUrl, setDeleteEventUrl] = useState<string[]>([])
+    const [deleteBannerUrl, setDeleteBannerUrl] = useState<string[]>([])
+
+    const [photoArraySrcBanner, setPhotoArraySrcBanner] = useState<string[]>([])
+    const [photoArrayFileBanner, setPhotoArrayFileBanner] = useState<File[]>([])
+
+    const [input_name, setInput_name] = useState(props.event.name)
+    const [input_description, setInput_description] = useState(props.event.description)
+    const [input_startDate, setInput_startDate] = useState(props.event.startDate.toISOString())
+    const [input_endDate, setInput_endDate] = useState(props.event.endDate.toISOString())
+    const [type, setType] = useState(props.event.type)
+
+    const [isChangeEventUrl, setIsChangeEventUrl] = useState(false)
+    const [isChangeBannerUrl, setIsChangeBannerUrl] = useState(false)
+
+    const [IsCreateEvent, setIsCreateEvent] = useState(false)
+
+    useEffect(() => {
+        setPhotoArraySrcEvent([props.event.photoUrl])
+        setPhotoArraySrcBanner([props.event.bannerUrl])
+        setInput_name(props.event.name)
+        setInput_description(props.event.description)
+        setInput_startDate(formatDate(props.event.startDate.toISOString()))
+        setInput_endDate(formatDate(props.event.endDate.toISOString()))
+        setType(props.event.type)
+    }, [props.event]);
+
+
+    useEffect(() => {
+        if (IsCreateEvent) {
+            const startDate = new Date(input_startDate)
+            const endDate = new Date(input_endDate)
+
+            props.EditEventThunk(eventUrl ,input_name, isChangeEventUrl, photoArrayFileEvent[0],
+                isChangeBannerUrl, photoArrayFileBanner[0], input_description,
+                startDate, endDate, router)
+        }
+        setIsCreateEvent(false)
+    }, [IsCreateEvent]);
+
+
+    useEffect(() => {
+        if (deleteEventUrl.length > 0) {
+            setIsChangeEventUrl(true)
+        }
+
+    }, [deleteEventUrl]);
+
+    useEffect(() => {
+        if (deleteBannerUrl.length > 0) {
+            setIsChangeBannerUrl(true)
+        }
+    }, [deleteBannerUrl]);
+
+
     if (props.event.name !== '') {
-        const eventUrl = usePathname().split('/')[usePathname().split('/').length - 1]
-
-        const [photoArraySrcEvent, setPhotoArraySrcEvent] = useState<string[]>([])
-        const [photoArrayFileEvent, setPhotoArrayFileEvent] = useState<File[]>([])
-        const [deleteEventUrl, setDeleteEventUrl] = useState<string[]>([])
-        const [deleteBannerUrl, setDeleteBannerUrl] = useState<string[]>([])
-
-        const [photoArraySrcBanner, setPhotoArraySrcBanner] = useState<string[]>([])
-        const [photoArrayFileBanner, setPhotoArrayFileBanner] = useState<File[]>([])
-
-        const [input_name, setInput_name] = useState(props.event.name)
-        const [input_description, setInput_description] = useState(props.event.description)
-        const [input_startDate, setInput_startDate] = useState(props.event.startDate.toISOString())
-        const [input_endDate, setInput_endDate] = useState(props.event.endDate.toISOString())
-        const [type, setType] = useState(props.event.type)
-
-        const [isChangeEventUrl, setIsChangeEventUrl] = useState(false)
-        const [isChangeBannerUrl, setIsChangeBannerUrl] = useState(false)
-
-        const [IsCreateEvent, setIsCreateEvent] = useState(false)
-
-        useEffect(() => {
-            setPhotoArraySrcEvent([props.event.photoUrl])
-            setPhotoArraySrcBanner([props.event.bannerUrl])
-            setInput_name(props.event.name)
-            setInput_description(props.event.description)
-            setInput_startDate(formatDate(props.event.startDate.toISOString()))
-            setInput_endDate(formatDate(props.event.endDate.toISOString()))
-            setType(props.event.type)
-        }, [props.event]);
-
-
-        useEffect(() => {
-            if (IsCreateEvent) {
-                const startDate = new Date(input_startDate)
-                const endDate = new Date(input_endDate)
-
-                props.EditEventThunk(eventUrl ,input_name, isChangeEventUrl, photoArrayFileEvent[0],
-                    isChangeBannerUrl, photoArrayFileBanner[0], input_description,
-                    startDate, endDate, router)
-            }
-            setIsCreateEvent(false)
-        }, [IsCreateEvent]);
-
-
-        useEffect(() => {
-            if (deleteEventUrl.length > 0) {
-                setIsChangeEventUrl(true)
-            }
-
-        }, [deleteEventUrl]);
-
-        useEffect(() => {
-            if (deleteBannerUrl.length > 0) {
-                setIsChangeBannerUrl(true)
-            }
-        }, [deleteBannerUrl]);
-
         return <CreateEventComponent photoArraySrcEvent={photoArraySrcEvent} photoArrayFileEvent={photoArrayFileEvent}
                                      setPhotoArrayFileEvent={setPhotoArrayFileEvent}
                                      setPhotoArraySrcEvent={setPhotoArraySrcEvent}

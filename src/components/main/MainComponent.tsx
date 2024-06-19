f39d44d297c6857c7e47c0370068e083f7bab24d
korobasssss <an.korobeynikova@gmail.com>
2024-06-19 11:48:10 +0300

    add banners to main page

--- src/components/main/MainComponent.tsx
+++ src/components/main/MainComponent.tsx
@@ -1,15 +1,15 @@
 import main_scss from '@/scss/components/main/Main.module.scss'
 
-import {BannerComponent} from "@/components/main/elements/banner/BannerComponent";
 import {ActionComponent} from "@/components/main/elements/actions/ActionComponent";
 import {ArtistsMainContainer} from "@/components/main/elements/artists/ArtistsMainContainer";
 import {MainArtsContainer} from "@/components/main/elements/arts/MainArtsContainer";
 import {MainAuctionsContainer} from "@/components/main/elements/auctions/MainAuctionsContainer";
+import {MainBannerContainer} from "@/components/main/elements/banner/MainBannerContainer";
 
 export const MainComponent = () => {
     return (
         <section className={main_scss.root}>
-            <BannerComponent/>
+            <MainBannerContainer/>
             <MainArtsContainer/>
             <ArtistsMainContainer/>
             <MainAuctionsContainer/>
--- src/components/main/elements/banner/BannerComponent.tsx
+++ src/components/main/elements/banner/BannerComponent.tsx
@@ -1,14 +1,23 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useRef} from "react";
 
 import prev_icon from '@/assets/icons/main/prev.svg'
 import next_icon from '@/assets/icons/main/next.svg'
 import Image from "next/image";
 
 import banner_scss from '@/scss/components/main/main_page/Banner.module.scss'
+import {EventsCategoriesInterface} from "@/interfaces/eventInterface";
+import {reformatDateFull} from "../../../../../utils/tests";
 
-export const BannerComponent = () => {
-    const bannersArr = ['/banner1.svg', '/banner2.svg']
-    const bannersArrMobile = ['/banner1_mobile.svg', '/banner2_mobile.svg']
+interface banner {
+    events: EventsCategoriesInterface[]
+    GetEvents(): void
+}
+
+export const BannerComponent = (props: banner) => {
+
+    useEffect(() => {
+        props.GetEvents()
+    }, []);
 
     const BannerRef = useRef<HTMLUListElement>(null);
     const BannerRef_mobile = useRef<HTMLUListElement>(null);
@@ -51,44 +60,39 @@ export const BannerComponent = () => {
         }
     };
 
-    return (
-        <section className={banner_scss.root}>
-            <section className={banner_scss.buttons_section}>
-                <button className={'button_main_page'}
-                        onClick={scrollToPreviousPhoto}>
-                    <Image src={prev_icon} alt={'prev_icon'} width={0} height={0}/>
-                </button>
-                <button className={'button_main_page'}
-                        onClick={scrollToNextPhoto}>
-                    <Image src={next_icon} alt={'next_icon'} width={0} height={0}/>
-                </button>
+    if (props.events !== undefined) {
+        return (
+            <section className={banner_scss.root}>
+                <section className={banner_scss.buttons_section}>
+                    <button className={'button_main_page'}
+                            onClick={scrollToPreviousPhoto}>
+                        <Image src={prev_icon} alt={'prev_icon'} width={0} height={0}/>
+                    </button>
+                    <button className={'button_main_page'}
+                            onClick={scrollToNextPhoto}>
+                        <Image src={next_icon} alt={'next_icon'} width={0} height={0}/>
+                    </button>
+                </section>
+                <ul className={banner_scss.ul} ref={BannerRef}>
+                    {props.events.map((events, index) => {
+                        return (
+                            <li className={banner_scss.main} key={index}>
+                                <img src={events.bannerUrl} className={banner_scss.banner_img}
+                                     alt={'banner'} crossOrigin={'anonymous'}/>
+                                <section className={banner_scss.buttons_section}>
+                                    <div className={banner_scss.date}>
+                                        {reformatDateFull(events.startDate.toISOString())} — {reformatDateFull(events.endDate.toISOString())}
+                                    </div>
+                                    <button className={'button_main_page-2 ' + banner_scss.button}>
+                                        Подробнее
+                                    </button>
+                                </section>
+                            </li>
+                        )
+                    })}
+                </ul>
+
             </section>
-            <ul className={banner_scss.ul} ref={BannerRef}>
-                {bannersArr.map((banner, index) => {
-                    return (
-                        <li className={banner_scss.main} key={index}>
-                            <img src={banner} className={banner_scss.banner_img}
-                                 alt={'banner'} crossOrigin={'anonymous'}/>
-                        </li>
-                    )
-                })}
-            </ul>
-            <ul className={banner_scss.ul_mobile} ref={BannerRef_mobile}>
-                {bannersArrMobile.map((banner, index) => {
-                    return (
-                        <li className={banner_scss.main} key={index}>
-                            <img src={banner} className={banner_scss.banner_img}
-                                 alt={'banner'} crossOrigin={'anonymous'}/>
-                        </li>
-                    )
-                })}
-            </ul>
-            <section className={banner_scss.buttons_section}>
-                <div className={banner_scss.date}>26 апреля — 10 мая</div>
-                <button className={'button_main_page-2 ' + banner_scss.button}>
-                    Подробнее
-                </button>
-            </section>
-        </section>
-    )
+        )
+    }
 }
